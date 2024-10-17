47cdb7fe126bb05d41d9399ea21fe3f82a7d9015
Stringpappy <jejeodunayo70@gmail.com>
2024-10-17 14:49:04 -0700

    sql advance

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
---mysql code that Creates a table with unique users.
+--MYSQL Creates a table with unique users.
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
