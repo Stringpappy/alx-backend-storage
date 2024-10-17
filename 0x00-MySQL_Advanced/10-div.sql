71e414d4bbd0e59df8919e0920c03737f6912901
Stringpappy <jejeodunayo70@gmail.com>
2024-10-17 16:08:49 -0700

    sql advance

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,14 +1,14 @@
--- script that create func safe div that divide numbers
+-- script that divide two num
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
-    DECLAR outcome FLOAT DEFAULT 0;
+    DECLARE result FLOAT DEFAULT 0;
 
     IF b != 0 THEN
-        SET outcome = a / b;
+        SET result = a / b;
     END IF;
-    RETURN outome;
+    RETURN result;
 END $$
 DELIMITER ;
