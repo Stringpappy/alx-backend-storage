4224b7481101cd19a7a8fb8486b5d86b63e2fa37
Stringpappy <jejeodunayo70@gmail.com>
2024-10-17 16:12:26 -0700

    sql advance

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,5 @@
--- iMysql scriptCreates a view need_meeting that lists all student
---that have a score  under 80 (strict) and no last_meeting or more than 1 mont
+--SQL script that creates a view need_meeting that
+--lists all students that have a score under 80 
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
     SELECT name
