179448cc3ab0f09b968e426f574619597dd8badf
Stringpappy <jejeodunayo70@gmail.com>
2024-10-17 12:39:36 -0700

    sql advance

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- a SQL script that creates a table users
 CREATE TABLES IF NOT EXISTS users (
-    id NULL autoincrement,
+    id NULL PRIMARY KEY AUTOINCREMENT,
     email VARCHAR(255),
     name VARCHAR(255)
-)
+);
