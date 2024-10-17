951db1e182bcaada2649743e9b5aebe1c75f7d04
Stringpappy <jejeodunayo70@gmail.com>
2024-10-17 15:23:19 -0700

    sql advance

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,5 @@
---MYSQiL Creates a table with unique users.
+--YSQiL Creates a table with unique users.
+-- Creates a table with unique users.
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
---SQL script that lists all bands with Glam rock as their main style,
---ranked by their longevity
-select band_name, coalesce(split, 2022) - formed as lifespan from metal_bands
-where style like '%Glam rock%' order by lifespan desc;
+-- sql scriptthat list all bands with glam rock
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
