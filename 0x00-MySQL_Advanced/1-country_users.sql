73aac6b51f06dac3da7966f94160de6a8e1df5e1
Stringpappy <jejeodunayo70@gmail.com>
2024-10-19 14:00:00 -0700

    sql advance

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,7 @@
---YSQiL Creates a table with unique users.
--- Creates a table with unique users.
-
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- SQL script that Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
