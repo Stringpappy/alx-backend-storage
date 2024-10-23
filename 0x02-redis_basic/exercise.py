e59dba6555c0264905996688726484e6dca90851
stringpappy <jejeodunayo70@gmail.com>
2024-10-23 22:30:30 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,29 +1,17 @@
-#!/usr/bin/env python3
-"""
-pythom module that uses Redis NoSQL data storage.
-"""
-import uuid
+#!/usr/bin/python3
+""""""
 import redis
+import uuid
 from typing import Union
 
-
 class Cache:
-    """
-    a func that represents an object for storing data in a Redis data storage.
-    """
-    def __init__(self) -> None:
-        """
-        Initialize the Cache instance.
-        """
+    def __init__(self):
+        """"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
-    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        a func that Stores a value in a Redis data storage and returns the key
-        """
-        dkey = str(uuid.uuid4())
-        self._redis.set(dkey, data)
-        return dkey
+        """"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
