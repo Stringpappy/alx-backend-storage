7e6dfd1e040270ff9ba2bdb5553f35190c8eb4a6
stringpappy <jejeodunayo70@gmail.com>
2024-10-24 00:11:55 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,3 +24,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: /
+            Optional[Callable[[bytes], Any]] = None) -> Optional[Any]:
+        """Retrieves data from Redis by key
+        and applies an optional conversion functiom.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieves data as a UTF-8 string from Redis.
+        """
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieves data as an integer from Redis
+        """
+        return self.get(key, fn=lambda x: int(x))
